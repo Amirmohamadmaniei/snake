d7c9c75a61f34fc57870c0741709b2a88f3d1bc1
Amir Mohamad Maniei <110853819+Amirmohamadmaniei@users.noreply.github.com>
2023-05-11 10:14:07 +0330

    Version 1

--- snake/settings.py
+++ snake/settings.py
@@ -7,9 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hl9(zzk#bd@!spc-yuom553(u9(v^h7(sw6n#r%ft=$%g4ule5'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
